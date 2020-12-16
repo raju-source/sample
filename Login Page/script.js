251562ce49a4ea166c0f3dc959dc537e4a67fb33
raju-source <76105363+raju-source@users.noreply.github.com>
2020-12-16 17:45:58 +0530

    Add files via upload

---
+++ Login Page/script.js
@@ -0,0 +1,27 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 8 || pwd.length > 8)
+		{
+			alert("Password min and max length is 8.");
+		}
+		else
+		{
+	alert('Thank You for Login & You are Redirecting to RJ Infra Projects');
+       window.location = "C:/Users/Raju/Desktop/Login%20Page/RJ/index.html";
+			}
+	}		
